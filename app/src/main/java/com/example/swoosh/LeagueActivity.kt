b7f75539014b906d247453cd9684f6d4255ce7a9
phoenix <phillip.addams@gmail.com>
2021-08-09 21:20:18 -0400

    League logic

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import android.widget.ToggleButton
 
 class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,40 @@ class LeagueActivity : BaseActivity() {
         setContentView(R.layout.activity_league)
     }
 
+    var selectedLeague = ""
+
+    val mensLeagueBtn: ToggleButton by lazy { findViewById(R.id.mensLeagueBtn) }
+    val womensLeagueBtn: ToggleButton by lazy { findViewById(R.id.womensLeagueBtn) }
+    val coedLeagueBtn: ToggleButton by lazy { findViewById(R.id.coedLeagueBtn) }
+
+    fun onMensClick(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClick(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClick(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "coed"
+    }
+
     fun leagueNextClick(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
     }
 }
